7984cfc7292153cb95cacaa0ad33cc36ca29afa6
Arjun Airen <arjunairen@icloud.com>
2022-07-12 15:30:39 -0400

    Adding dataset and R file

---
+++ loadingData.R
@@ -0,0 +1,21 @@
+#### Load in csv file ####
+x <- 5
+
+student_data <- read.csv("data/students_performance.csv")
+View(student_data)
+
+## Practice plotting with your dataset
+
+ggplot(data = student_data, aes(x = gender, y = math.score)) +
+  geom_bar(stat = "summary",
+             fun = "mean") +
+  labs(title = "Average Math Score vs Gender",
+       x = "Gender",
+       y = "Math Score")
+
+ggplot(data = student_data, aes(x = math.score, y = reading.score, color = gender)) +
+  geom_point() +
+  labs(title = "Reading Scores vs Math Scores of Students",
+       x = "Math Score",
+       y = "Reading Score",
+       color = "Sex")
