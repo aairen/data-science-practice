63808e301f44576da856fe58074a730f2e90a04d
Arjun Airen <arjunairen@icloud.com>
2022-07-13 16:05:30 -0400

    created exploratory_analysis file

---
+++ exploratory_analysis.R
@@ -0,0 +1,15 @@
+## Practice plotting with your dataset
+
+ggplot(data = student_data, aes(x = gender, y = math.score)) +
+  geom_bar(stat = "summary",
+           fun = "mean") +
+  labs(title = "Average Math Score vs Gender",
+       x = "Gender",
+       y = "Math Score")
+
+ggplot(data = student_data, aes(x = math.score, y = reading.score, color = gender)) +
+  geom_point() +
+  labs(title = "Reading Scores vs Math Scores of Students",
+       x = "Math Score",
+       y = "Reading Score",
+       color = "Sex")
